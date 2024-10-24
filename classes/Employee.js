e66309569a6110043b35de64f98382491c9267fa
David Dixon <daviddi@mo.co.uk>
2024-10-24 09:58:12 +0100

    cc set up

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,5 +1,26 @@
 class Employee {
-
+    #salary
+    #isHired
+constructor(name, position, salary, isHired) {
+    this.name = name
+    this.position = position
+    this.#salary = salary
+    this.#isHired = isHired
+}
+getSalary() {
+    return this.#salary
+}
+setSalary(amount) {
+    return this.#salary += amount
+}
+getStatus() {
+    return this.#isHired
+}
+setStatus(command) {
+    if (command == "hire") {
+        this.#isHired = true
+    } else {this.#isHired = false}
+}
 
 
     
